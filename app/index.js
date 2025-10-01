6c60ef9ee56b1cedf32764b24344d3f83d92110e
ISHIKA MANDAL <s223973381@deakin.edu.au>
2025-10-01 10:00:13 +1000

    test CI pipeline

--- app/index.js
+++ app/index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("🚀 Hello from Awesome DevOps Pipeline!");
+  res.send("🚀 Hello from Awesome DevOps Pipeline!!");
 });
 
 app.listen(PORT, () => {
