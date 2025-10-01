27a22c4fb35d7f9bd5363ec5a6df3ab1d2969c80
ISHIKA MANDAL <s223973381@deakin.edu.au>
2025-10-01 11:13:19 +1000

    test staging

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,3 +4,6 @@ variable "acr_login_server" {}
 variable "acr_username" {}
 variable "acr_password" {}
 variable "subscription_id" {}
+variable "client_id" {}
+variable "client_secret" {}
+variable "tenant_id" {}
