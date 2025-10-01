470081da139af15cb5e123adf1cc8f656defe666
ISHIKA MANDAL <s223973381@deakin.edu.au>
2025-10-01 14:03:09 +1000

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,3 +7,7 @@ variable "subscription_id" {}
 variable "client_id" {}
 variable "client_secret" {}
 variable "tenant_id" {}
+variable "image_tag" {
+  description = "The Docker image tag to deploy (commit SHA or staging/production)"
+  type        = string
+}
