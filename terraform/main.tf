13a07800a00e28c970b325351ef99cf995534fbf
ISHIKA MANDAL <s223973381@deakin.edu.au>
2025-10-01 11:26:13 +1000

    testing

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,21 +19,22 @@ resource "azurerm_service_plan" "plan" {
   sku_name            = "B1"
 }
 
-
-resource "azurerm_app_service" "app" {
+resource "azurerm_linux_web_app" "app" {
   name                = "awesome-devops-${var.env}-app"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  app_service_plan_id = azurerm_service_plan.plan.id
+  service_plan_id     = azurerm_service_plan.plan.id
 
   site_config {
-    linux_fx_version = "DOCKER|${var.image_name}"
+    application_stack {
+      docker_image     = var.image_name
+      docker_registry_url = "https://${var.acr_login_server}"
+      docker_registry_username = var.acr_username
+      docker_registry_password = var.acr_password
+    }
   }
 
   app_settings = {
     WEBSITES_ENABLE_APP_SERVICE_STORAGE = "false"
-    DOCKER_REGISTRY_SERVER_URL          = "https://${var.acr_login_server}"
-    DOCKER_REGISTRY_SERVER_USERNAME     = var.acr_username
-    DOCKER_REGISTRY_SERVER_PASSWORD     = var.acr_password
   }
 }
