ae9c70f8e087786bc9fdf10058f987a96bee83cd
ISHIKA MANDAL <s223973381@deakin.edu.au>
2025-10-02 14:15:28 +1000

    test staging

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,38 +6,32 @@ provider "azurerm" {
   tenant_id       = var.tenant_id
 
   resource_provider_registrations = "none"
-
 }
 
-
-resource "azurerm_resource_group" "rg" {
-  name     = "awesome-devops-${var.env}-rg"
-  location = "Australia East"
-}
+# Remove RG resource, since it's pre-created manually
 
 resource "azurerm_service_plan" "plan" {
   name                = "awesome-devops-${var.env}-plan"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = "Australia East"
+  resource_group_name = "awesome-devops-${var.env}-rg"
   os_type             = "Linux"
   sku_name            = "B1"
 }
 
 resource "azurerm_linux_web_app" "app" {
   name                = "awesome-devops-${var.env}-app"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = "Australia East"
+  resource_group_name = "awesome-devops-${var.env}-rg"
   service_plan_id     = azurerm_service_plan.plan.id
 
-site_config {
-  application_stack {
-    docker_image_name        = "${var.acr_login_server}/awesome-devops-app:${var.image_tag}"
-    docker_registry_url      = "https://${var.acr_login_server}"
-    docker_registry_username = var.acr_username
-    docker_registry_password = var.acr_password
+  site_config {
+    application_stack {
+      docker_image_name        = "${var.acr_login_server}/awesome-devops-app:${var.image_tag}"
+      docker_registry_url      = "https://${var.acr_login_server}"
+      docker_registry_username = var.acr_username
+      docker_registry_password = var.acr_password
+    }
   }
-}
-
 
   app_settings = {
     WEBSITES_ENABLE_APP_SERVICE_STORAGE = "false"
